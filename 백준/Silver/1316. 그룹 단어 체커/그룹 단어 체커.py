65373194eb088be2bd1ad4308b60781845a32b43
accio <92027143+accio3014@users.noreply.github.com>
2023-01-28 19:54:57 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,11 @@
+N = int(input())
+count = N
+
+for i in range(N):
+    word = input()
+    for index in range(len(word) - 1):            # 단어의 처음부터 비교, -1을 한 이유는 현재 위치와 다음 위치를 비교할 예정
+        if(word[index] != word[index + 1]):       # 현재위치의 알파벳과 다음 위치의 알파펫이 다를 경우 , 그룹단어인 경우
+            if(word[index + 1] in word[:index]):  # 다음 위치의 알파벳이 현재위치 이전에 알파벳에 있을 경우, 즉 그룹단어가 아님
+                count -= 1
+                break
+print(count)
